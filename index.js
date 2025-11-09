b0517a521c1ad5557185352d9a73dd2fdffd1b20
Rabin Khandakar <rabinbd22@gmail.com>
2025-11-09 22:57:22 +0600

    Clean Connect Server index.js initiated

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) => {
+  res.send('Clean Connect Server is Running');
+});
+
+app.listen(port, () => {
+  console.log(`Clean Connect Server is Running on port: ${port}`);  
+});
